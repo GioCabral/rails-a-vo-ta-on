22f2e4d5581c3cc68d7d569054413eed8967e4a2
giovanni cabral <giolucacab@outlook.com>
2022-12-08 10:53:13 -0300

    retirado token de receive

--- app/controllers/billings_controller.rb
+++ app/controllers/billings_controller.rb
@@ -1,5 +1,6 @@
 class BillingsController < ApplicationController
   protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
   skip_before_action :authenticate_user!
   def receive
     payment_id = params[:data][:id]
